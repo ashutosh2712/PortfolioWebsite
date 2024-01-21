97134830a2b5452244e7c9b91f16f7037693548a
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-22 05:21:04 +0530

    Footer Section done.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+
+import { RiLinkedinBoxFill } from "react-icons/ri";
+import { SiGithub } from "react-icons/si";
+import { SiCodechef } from "react-icons/si";
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer>
+      <a href="#" className="footerLogo">
+        Ashutosh
+      </a>
+      <ul className="permaLinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skill">Skill</a>
+        </li>
+        <li>
+          <a href="#experince">Experience</a>
+        </li>
+        <li>
+          <a href="#project">Projects</a>
+        </li>
+        <li>
+          <a href="#education">Education</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footerSocials">
+        <a href="https://www.linkedin.com/in/ashutosh-kumar-roy-8577a81b8/">
+          <RiLinkedinBoxFill />
+        </a>
+        <a href="https://github.com/ashutosh2712">
+          <SiGithub />
+        </a>
+        <a href="https://www.codechef.com/users/ashutosh2712">
+          <SiCodechef />
+        </a>
+      </div>
+      <div className="footerCopyright">
+        <small>copyright &copy;Ashutosh kumar Roy.All rights reserved. </small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
