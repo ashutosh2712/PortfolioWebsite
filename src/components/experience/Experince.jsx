8e87c9d9c7366a549fd57ccf4c1f96882fe613d9
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-22 15:25:37 +0530

    Experience Section done.

--- src/components/experience/Experince.jsx
+++ src/components/experience/Experince.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import "./experience.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import experiences from "../../experiences";
+
 const Experince = () => {
-  return <section id="experince">Experience</section>;
+  return (
+    <section id="experince">
+      <h5>Experience</h5>
+      <h2>Industry Experience</h2>
+      <div className="experienceContainer">
+        <VerticalTimeline lineColor="">
+          {experiences.map((experience) => (
+            <VerticalTimelineElement
+              key={experience.Id}
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "var(--color-primary)",
+                color: "var(--color-white)",
+              }}
+              contentArrowStyle={{
+                borderRight: "7px solid  var(--color-primary)",
+              }}
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                color: "var(--color-white)",
+              }}
+              date={experience.date}
+              icon={experience.icon}
+            >
+              <h3>{experience.title}</h3>
+              <h5>{experience.location}</h5>
+              <ul className="tick-list">
+                {experience.description.map((des, index) => (
+                  <li key={index}>{des}</li>
+                ))}
+              </ul>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </section>
+  );
 };
 
 export default Experince;
---
+++ src/experiences.jsx
@@ -0,0 +1,19 @@
+import { SiIntel } from "react-icons/si";
+const Intel = [];
+const experiences = [
+  {
+    Id: 1,
+    title: "Full Stack Developer Intern",
+    date: "July 2022 - June 2023",
+    icon: <SiIntel />,
+    location: "Bangalore, India",
+    description: [
+      "We developed a Full-stack application for the Data Center and AI group in a team of 8 members.",
+      "It is a Web portal developed using front-end technologies like HTML, CSS, Bootstrap, jinja template, and vanilla JavaScript and back-end technologies like Python Flask, SQLalchemy, and PostgreSQL.",
+      "The Portal represents a cross-intel initiative to drive systematic market-important workload orientation into Intel's product planning, development, and delivery.",
+      "Influenced 250 enterprise data innovators and doubled Intel’s consumption revenue to 60M US Dollars.",
+    ],
+  },
+];
+
+export default experiences;
